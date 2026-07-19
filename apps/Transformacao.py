2f68f6186660e9baac602c3d0263a1140f59f822
Gabriel Jorge <jorginghfvh14@gmail.com>
2026-07-19 14:56:18 -0300

    Adiciona a tabela 'vendas_por_ano' em output

---
+++ apps/Transformacao.py
@@ -0,0 +1,14 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import year
+
+if __name__ == "__main__":
+    spark = SparkSession.builder.appName("Aplicação Transformacao").getOrCreate()
+    spark.sparkContext.setLogLevel("ERROR")
+
+    schema = "id INT, nome STRING, status STRING, cidade STRING, vendas INT, data STRING"
+
+    despachantes = spark.read.csv("/app/data/despachantes.csv", header=False, schema=schema)
+
+    resultado = despachantes.select("data").groupBy(year("data").alias("ano")).count()
+    resultado.write.mode("overwrite").parquet("/app/output/vendas_por_ano")
+    spark.stop()
